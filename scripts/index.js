8a8e7b299716f70ba8ed276d48ffea4d75b65e5c
david robison <mrdavidrobison@gmail.com>
2017-07-20 14:26:52 -0600

    grab initial values from inputs

---
+++ scripts/index.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+
+  var firstName = $("#first_name");
+  var firstNameTransfer = firstName.val();
+
+  var lastName = $("#last_name");
+  var lastNameTransfer = firstName.val();
+
+  var emailAdd = $("#email");
+  var emailAddTransfer = firstName.val();
+
+  var password = $("#password");
+  var passwordTransfer = firstName.val();
+
+})
